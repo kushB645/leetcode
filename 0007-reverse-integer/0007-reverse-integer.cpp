a69bf4e5fe3f06e54ca15a2333b663c587ef88c3
Kush Bhardwaj <kushb545@gmail.com>
2026-03-15 22:48:58 +0530

    Time: 2 ms (38.04%), Space: 8.7 MB (20.02%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long rev = 0;
+        while (x != 0) {
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            if (rev > INT_MAX || rev < INT_MIN)
+                return 0; 
+            x /= 10;
+        }
+        return (int)rev;
+    }
+};
