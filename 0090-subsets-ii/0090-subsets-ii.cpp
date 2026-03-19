605c99e5693957004c5d72197d1b6a0d56cabc3e
Kush Bhardwaj <kushb545@gmail.com>
2026-03-19 13:22:54 +0530

    Time: 3 ms (32.44%), Space: 10.3 MB (84.12%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(int index,vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans){
+    ans.push_back(temp);
+     for (int i = index; i < nums.size(); i++) {
+            // skip duplicates
+            if (i > index && nums[i] == nums[i - 1]) continue;
+
+            temp.push_back(nums[i]);
+            solve(i + 1, nums, temp, ans);
+            temp.pop_back();
+        }
+};
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        vector<vector<int>> ans;
+
+        solve(0, nums, temp, ans);
+        return ans;
+    }
+};
