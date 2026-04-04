a27eccb14bd89b7d2226902a30a9cf2a36171f4c
Kush Bhardwaj <kushb545@gmail.com>
2026-04-04 10:56:50 +0530

    Time: 40 ms (90.55%), Space: 29.1 MB (72.14%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ls;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-        {
-           if(i>0 && nums[i]==nums[i-1]) continue;
-           int j=i+1;
-           int k=nums.size()-1;
-           while(j<k){
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum<0)
-            {
-                j++;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for (int last = nums.size() - 1; last >= 2; last--) {
+            int left = 0;
+            int right = last - 1;
+
+            if (last < nums.size() - 1 && nums[last] == nums[last + 1]) {
+                continue;
             }
-            else if(sum>0)
-            {
-                k--;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[last];
+                if (sum == 0) {
+                    ans.push_back({nums[left], nums[right], nums[last]});
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+
+                else if (sum < 0) {
+                    left++;
+                }
+
+                else {
+                    right--;
+                }
             }
-            else{
-                vector<int>temp={nums[i],nums[j],nums[k]};
-                ls.push_back(temp);
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1]) j++;
-                while(j<k && nums[k]==nums[k+1]) k--;
-            }
-           }
         }
-        return ls;
+        return ans;
     }
 };
